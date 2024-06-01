ef7c9ea07031aa76a19a6fdfacef60ba1e2c9a95
Polin Wei <polin.wei@gmail.com>
2024-06-01 17:43:11 +0800

    api controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
